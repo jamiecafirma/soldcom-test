9181a7704791936d1eb2a1c0737fe64c0d680f70
Jamie Cafirma <jamie.cafirma@gmail.com>
2021-12-01 15:29:26 +0000

    first commit

---
+++ soldcom-todomvc-test-starter/cypress/integration/sold.com/persistence.spec.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+// type definitions for custom commands like "createDefaultTodos"
+/// <reference types="../../support" />
+
+describe('SOLD.com Test Project: persistent', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+});
---
+++ soldcom-todomvc-test-starter/cypress/integration/sold.com/tasks-management.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+// type definitions for custom commands like "createDefaultTodos"
+/// <reference types="../../support" />
+
+// ----------------------------------------------------------------------------
+// Custom Commands:
+//   command defined in /integration/sold.com/support/commands.js
+// ----------------------------------------------------------------------------
+
+describe('SOLD.com Test Project: task management', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+});
