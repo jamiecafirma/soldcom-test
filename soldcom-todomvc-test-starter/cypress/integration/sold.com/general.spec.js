818781714a42d1f5bb81ac1b4c25b57328d047de
Jamie Cafirma <jamie.cafirma@gmail.com>
2021-12-01 08:18:47 -0800

    Test 3: Add todo, should clear text input field when an item is added, assert field is blank

--- soldcom-todomvc-test-starter/cypress/integration/sold.com/general.spec.js
+++ soldcom-todomvc-test-starter/cypress/integration/sold.com/general.spec.js
@@ -15,10 +15,18 @@ describe('SOLD.com Test Project: general', () => {
 
   it('hides main and footer sections', () => {
     cy.get('.main').should('not.exist')
+
     cy.get('.footer').should('not.exist')
   })
 
   it ('focuses on the todo input field', () => {
     cy.focused().should('have.class', 'new-todo')
+  })
+
+  it ('clears text-input field when an item is added', () => {
+    cy.get('.new-todo')
+    .type('walk the dogs')
+    .type('{enter}')
+    .should('have.value', '')
   })
 });
